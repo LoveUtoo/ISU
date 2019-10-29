8ee70afd638ac19ddc372e1c052b2771d2bdfe94
LoveUtoo <nvcoballe@gmail.com>
2019-10-29 09:13:52 -0400

    stuff

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let x = 50;
+let left = true;
+let cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  cnv = createCanvas(500, 500);
+  cnv.parent('sketch-holder');
+  //cnv.center();
+  background(100);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+	background(100);
+	fill(255);	
+	
+  ellipse(x, 50, 80, 80);
+  if(left) {
+	x += 1;
+	if(x >= 500)
+		left = false
+  }
+  if(!left) {
+	x -= 1;
+	if (x <=50){
+		left = true;
+	}
+  }
 }
