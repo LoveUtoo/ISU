214da59c34869c211eee593294e63ff2643a567f
LoveUtoo <nvcoballe@gmail.com>
2019-11-12 11:09:10 -0500

    updated battleship.js

--- ISU Project/battleship.js
+++ ISU Project/battleship.js
@@ -35,31 +35,6 @@ function setup() { //setting up stuff
     cnv.parent('sketch-holder');
     background(0);
 
-    inputM1 = createInput('0', "number");
-    inputM1.size(50);
-    inputM1.position(SW - 220, 700);
-
-    inputB1 = createInput('0', "number");
-    inputB1.size(50);
-    inputB1.position(SW - 120, 700);
-
-    inputM2 = createInput('0', "number");
-    inputM2.size(50);
-    inputM2.position(SW - 20, 700);
-
-    inputB2 = createInput('0', "number");
-    inputB2.size(50);
-    inputB2.position(SW + 80, 700);
-
-    buttonFire = createButton("FIRE");
-    buttonFire.size(50);
-    buttonFire.position(SW + 140, 700);
-    buttonFire.mousePressed(fired);
-
-    buttonHelp = createButton("HELP");
-    buttonHelp.size(50);
-    buttonHelp.position(SW + 190, 700);
-
 }
 
 function windowResized() { //changes location of buttons depending on window size BECAUSE CREATEINPUT JUST CREATES AN HTML INPUT TAG!!!!!!
@@ -79,9 +54,9 @@ function draw() { //drawing stuff
     fill(255);
 
     textSize(16);
-    text(line1.maxY, 100, 100);
+    text(mouseX, 100, 100);
     text(line1.maxX, 100, 200);
-    text(line1.currX, 100, 300);
+    text(line2.maxX, 100, 300);
     text(20 + setInitialX(line1), 100, 400);
     //text(line2.currX, 150, 400);
     fill(255, 153, 51);
@@ -89,13 +64,13 @@ function draw() { //drawing stuff
     stroke(255, 153, 51);
     strokeWeight(1);
 
-    for (var i = 20; i < width; i += 50) {
-        for (var j = 20; j < height - 50; j += 50) {
-            line(i, 20, i, height - 80);
-            line(20, j, width, j);
-            text((j - 20) / 50, j - 5, height - 60);
+    for (var i = 25; i < width; i += 75) {
+        for (var j = 20; j < height; j += 75) {
+            line(i, 20, i, height - 30);
+            line(25, j, width - 25, j);
+            text((j - 20) / 75, j - 5, height - 10);
         }
-        text((height - 80 - i) / 50, 0, i + 5);
+        text((height - 25 - i) / 75, 0, i + 5);
     }
 
     if (line1.currX >= line1.maxX && line2.currX >= line2.maxX) {
@@ -107,34 +82,36 @@ function draw() { //drawing stuff
     //Actual drawing of lines
     stroke(255, 0, 0);
     fill(255, 0, 0);
-    line(20 + setInitialX(line1), 520 - (ifZero(line1.b) * 50), 20 + (line1.currX * 50), 520 - (line1.currY * 50)); //line 1
-    text("M1 = ", 0, 587);
-    text("B1 = ", 100, 587);
+    line(25 + setInitialX(line1), 770 - (ifZero(line1.b) * 75), 25 + (line1.currX * 75), 770 - (line1.currY * 75)); //line 1
     stroke(0, 0, 255);
     fill(0, 0, 255);
-    line(20 + setInitialX(line2), 520 - (ifZero(line2.b) * 50), 20 + (line2.currX * 50), 520 - (line2.currY * 50)); //line 2
-    text("M2 = ", 200, 587);
-    text("B2 = ", 300, 587);
+    line(25 + setInitialX(line2), 770 - (ifZero(line2.b) * 75), 25 + (line2.currX * 75), 770 - (line2.currY * 75)); //line 2
 
     ellipse(20 + (intX * 50), 520 - (intY * 50), 10, 10); //This should be in a separate function eventually if you want the ellipses to stay
 
 }
 
 function maxValue(l) {
-    if (l.m > 0) {
+    if (l.m > 0 && l.m * 10 + Number(l.b) <= 10) {
         l.maxY = 10;
         l.maxX = (Number(l.maxY) - Number(l.b)) / Number(l.m);
-    } else {
+    } else if (l.m > 0 && l.m * 10 + Number(l.b) > 10) {
+		l.maxX = 10;
+		l.maxY = l.m * 10 + Number(l.b);
+	} else if (l.m < 0 && l.m * 10 + Number(l.b) > 0) {
+		l.maxX = 10;
+		l.m * 10 + Number(l.b);
+	} else if (l.m < 0 && l.m * 10 + Number(l.b) <= 0) {
         l.maxY = 0;
         l.maxX = (Number(l.maxY) - Number(l.b)) / Number(l.m);
     }
 }
 
 function fired() {
-    line1.m = Math.round(inputM1.value());
-    line2.m = Math.round(inputM2.value());
-    line1.b = Math.round(inputB1.value());
-    line2.b = Math.round(inputB2.value());
+    line1.m = Math.round(document.getElementById('m1').value);
+    line2.m = Math.round(document.getElementById('m2').value);
+    line1.b = Math.round(document.getElementById('b1').value);
+    line2.b = Math.round(document.getElementById('b2').value);
     intX = (line2.b - line1.b) / (line1.m - line2.m);
     intY = line1.m * intX + Number(line1.b); //This cast to number to fix the stupid problem where b1 was a character/string due to stupid js typesafe shit
     maxValue(line1);
@@ -177,7 +154,7 @@ function ifZero(n) {
 
 function setInitialX(l) {
     if (l.b < 0) {
-        return ((0 - l.b) / l.m) * 50;
+        return ((0 - l.b) / l.m) * 75;
     } else {
         return 0;
     }
